995cf671c36fde86cef9169c85044e1fed7c11d1
Gaspard Le Claire <gaspardleclaire@gmail.com>
2024-04-10 22:19:02 +0200

    debug style conversation

--- public/js/message.js
+++ public/js/message.js
@@ -30,30 +30,33 @@ $('.boutonConversation').on('click', function () {
       data.forEach(message => {
         console.log(message);
         if (message.IDUTILISATEUR !== authUtilisateur) {
-          console.log('gauche')
-          $('.messages').append(`<div class="col-start-1 col-end-8 p-3 rounded-lg">
-                <div class="flex flex-row items-center">
-                  <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                    ${message.IDUTILISATEUR}
-                  </div>
-                  <div class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                    <div>${message.CONTENU}</div>
-                  </div>
-                </div>
-              </div>`)
-        }
-        else {
-          console.log("droite")
-          $('.messages').append(`<div class="col-start-6 col-end-13 p-3 rounded-lg">
-                    <div class="flex items-center justify-start flex-row-reverse">
-                      <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                      ${message.IDUTILISATEUR}
-                      </div>
-                      <div class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                        <div>${message.CONTENU}</div>
-                      </div>
-                    </div>
-                  </div>`)
+          console.log('gauche');
+          $('.messages').append(`
+          <div class="col-start-1 col-end-8 p-3 rounded-lg"> <!-- Message à gauche -->
+          <div class="flex flex-row items-center">
+            <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+            ${message.IDUTILISATEUR}
+            </div>
+            <div class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+              <div>${message.CONTENU}</div>
+            </div>
+          </div>
+        </div>
+          `);
+        } else {
+          console.log("droite");
+          $('.messages').append(`
+          <div class="col-start-10 col-end-13 p-3 rounded-lg justify-self-end"> <!-- Message à droite aligné tout à fait à droite -->
+          <div class="flex items-center justify-end flex-row-reverse">
+            <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+            ${message.IDUTILISATEUR}
+            </div>
+            <div class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+              <div>${message.CONTENU}</div>
+            </div>
+          </div>
+        </div>
+          `);
         }
       });
 
--- resources/views/messages/messagerie.blade.php
+++ resources/views/messages/messagerie.blade.php
@@ -8,7 +8,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"></path>
             </svg>
           </div>
-          <div class="ml-2 font-bold text-2xl">QuickChat</div>
+          <div class="ml-2 font-bold text-2xl">Conversations</div>
         </div>
 
         <!-- LIST CONVERSATION -->
@@ -46,51 +46,48 @@
           <div class="flex flex-col h-full overflow-x-auto mb-4">
             <div class="flex flex-col h-full">
               <div class="grid grid-cols-12 gap-y-2 messages">
-
-              </div>
-
-            </div>
-          </div>
-          <div class="flex flex-row items-center h-15 rounded-xl bg-white w-full px-4">
-            <div>
-              <button class="flex items-center justify-center text-gray-400 hover:text-gray-600">
-                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path>
-                </svg>
-              </button>
-            </div>
-            <div class="flex-grow ml-4">
-              <div class="relative w-full">
-                <form action="{{ route('message.store', [0,0,0]) }}" method="POST" id="form-new-message">
-                  @CSRF
-                  <input type="text" class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10" name="CONTENU" />
-                  <button type="submit" class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                    </svg>
-                  </button>
-
               </div>
             </div>
-            <div class="ml-4">
-              <button type="submit" class="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
-                <span>Send</span>
-                <span class="ml-2">
-                  <svg class="w-4 h-4 transform rotate-45 -mt-px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
+            <div class="flex flex-row items-center h-15 rounded-xl bg-white w-full px-4">
+              <div>
+                <button class="flex items-center justify-center text-gray-400 hover:text-gray-600">
+                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path>
                   </svg>
-                </span>
-              </button>
+                </button>
+              </div>
+              <div class="flex-grow ml-4">
+                <div class="relative w-full">
+                  <form action="{{ route('message.store', [0,0,0]) }}" method="POST" id="form-new-message">
+                    @CSRF
+                    <input type="text" class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10" name="CONTENU" />
+                    <button type="submit" class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
+                      <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                      </svg>
+                    </button>
+
+                </div>
+              </div>
+              <div class="ml-4">
+                <button type="submit" class="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
+                  <span>Send</span>
+                  <span class="ml-2">
+                    <svg class="w-4 h-4 transform rotate-45 -mt-px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
+                    </svg>
+                  </span>
+                </button>
+              </div>
+              </form>
             </div>
-            </form>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
-  <!-- Inclure MapLibre-GL avant le script -->
-  <script src="{{ asset('js/maplibre-gl.js') }}"></script>
-  <!-- Inclure votre fichier JavaScript après MapLibre-GL -->
-  <script src="{{ asset('js/message.js') }}"></script>
+    <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
+    <!-- Inclure MapLibre-GL avant le script -->
+    <script src="{{ asset('js/maplibre-gl.js') }}"></script>
+    <!-- Inclure votre fichier JavaScript après MapLibre-GL -->
+    <script src="{{ asset('js/message.js') }}"></script>
 </x-app-layout>
